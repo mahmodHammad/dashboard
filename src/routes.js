1779c29ff32672649bd12ff199a3179af7a557d3
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-07 17:53:32 +0200

    init

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.9.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Language from "@material-ui/icons/Language";
+// core components/views for Admin layout
+import DashboardPage from "views/Dashboard/Dashboard.js";
+
+// core components/views for RTL layout
+import RTLPage from "views/RTLPage/RTLPage.js";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "/rtl-page",
+    name: "RTL Support",
+    rtlName: "التحويل الي الانجليزيه",
+    icon: Language,
+    component: RTLPage,
+    layout: "/rtl",
+  }
+];
+
+export default dashboardRoutes;
